9c046aff029052661ba5346ebaf0450fb085061c
jerryguowei <jerryguowei@gmail.com>
2021-07-16 10:16:00 +1000

    letter combinations - 0017-backtrack

--- src/main/java/com/duduanan/leetcode/N0017_LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/duduanan/leetcode/N0017_LetterCombinationsOfAPhoneNumber.java
@@ -1,45 +1,81 @@
 package com.duduanan.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class N0017_LetterCombinationsOfAPhoneNumber {
 
+    private Map<Character, String> map = new HashMap<>();
+    {
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+    }
+    private List<String> result = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        for(int i = 0; i < digits.length(); i++){
-            int digit = Integer.parseInt("" + digits.charAt(i));
-            List<Character> list = getCharacterList(digit);
-            List<String> tempList = new ArrayList<>();
-            for(Character ch : list) {
-                String temp = "" + ch;
-                if(result.isEmpty()){
-                    tempList.add(temp);
-                } else {
-                    for(String st: result){
-                        tempList.add(st+temp);
-                    }
-                }
-            }
-            result = tempList;
+        if(digits.isEmpty()) {
+            return result;
         }
+        backtrack(digits, 0, new StringBuilder());
         return result;
     }
 
-    private List<Character> getCharacterList(int digit) {
-        List<Character> list = new ArrayList<>();
-        int num = 3;
-        if(digit == 7 || digit == 9) num = 4;
-        int start = 'a' + (digit - 2) * 3;
-        if(digit > 7){
-            start++; //7 has 7 nums;
+    public void backtrack(String digits, int i, StringBuilder sb) {
+        if(i == digits.length()){
+            result.add(sb.toString());
+            return;
         }
-
-        for(int i = 0; i < num; i++){
-            list.add((char)(start + i));
+        for(char c : map.get(digits.charAt(i)).toCharArray()) {
+            sb.append(c);
+            backtrack(digits, i + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
-        return list;
     }
+
+
+
+//    public List<String> letterCombinations(String digits) {
+//        List<String> result = new ArrayList<>();
+//        for(int i = 0; i < digits.length(); i++){
+//            int digit = Integer.parseInt("" + digits.charAt(i));
+//            List<Character> list = getCharacterList(digit);
+//            List<String> tempList = new ArrayList<>();
+//            for(Character ch : list) {
+//                String temp = "" + ch;
+//                if(result.isEmpty()){
+//                    tempList.add(temp);
+//                } else {
+//                    for(String st: result){
+//                        tempList.add(st+temp);
+//                    }
+//                }
+//            }
+//            result = tempList;
+//        }
+//        return result;
+//    }
+
+//    private List<Character> getCharacterList(int digit) {
+//        List<Character> list = new ArrayList<>();
+//        int num = 3;
+//        if(digit == 7 || digit == 9) num = 4;
+//        int start = 'a' + (digit - 2) * 3;
+//        if(digit > 7){
+//            start++; //7 has 7 nums;
+//        }
+//
+//        for(int i = 0; i < num; i++){
+//            list.add((char)(start + i));
+//        }
+//        return list;
+//    }
     public static void main(String[] args) {
         N0017_LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber17 = new N0017_LetterCombinationsOfAPhoneNumber();
         System.out.println(letterCombinationsOfAPhoneNumber17.letterCombinations("23"));
