91130a2c1a602136e4813eb4a46a3b4592a34f23
jerryguowei <jerryguowei@gmail.com>
2021-08-20 21:49:04 +1000

    RomanToInteger - 0013

--- src/main/java/com/duduanan/leetcode/N0013_RomanToInteger.java
+++ src/main/java/com/duduanan/leetcode/N0013_RomanToInteger.java
@@ -1,49 +1,30 @@
 package com.duduanan.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class N0013_RomanToInteger {
 
     public int romanToInt(String s) {
-        int result = 0;
-        int next = 0;
-        int curr = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(next != 0) {
-                curr = next;
-                next = 0;
-            } else {
-                curr = getNumber(s.charAt(i));
-            }
-            if(i + 1 < s.length()){
-                next = getNumber(s.charAt(i+1));
-                if(curr < next ){
-                    result += next - curr;
-                    i++;
-                    next = 0;
-                    continue;
-                }
-            }
-            result += curr;
-        }
-        return result;
-    }
+        int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
-    private int getNumber(char c) {
-        switch(c){
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            default: // 'M'
-                return 1000;
+        for (int i = 0; i < s.length(); i++) {
+            int temp = map.get(s.charAt(i));
+            if (i + 1 < s.length() && temp < map.get(s.charAt(i + 1))) {
+                res -= temp;
+            } else {
+                res += temp;
+            }
         }
+        return res;
     }
 
     public static void main(String[] args) {
@@ -55,16 +36,16 @@ public class N0013_RomanToInteger {
 /**
  * 13. Roman to Integer
  * Easy
- *
+ * <p>
  * 782
- *
+ * <p>
  * 72
- *
+ * <p>
  * Add to List
- *
+ * <p>
  * Share
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
- *
+ * <p>
  * Symbol       Value
  * I             1
  * V             5
@@ -74,42 +55,42 @@ public class N0013_RomanToInteger {
  * D             500
  * M             1000
  * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
- *
+ * <p>
  * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
- *
+ * <p>
  * I can be placed before V (5) and X (10) to make 4 and 9.
  * X can be placed before L (50) and C (100) to make 40 and 90.
  * C can be placed before D (500) and M (1000) to make 400 and 900.
  * Given a roman numeral, convert it to an integer.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: s = "III"
  * Output: 3
  * Example 2:
- *
+ * <p>
  * Input: s = "IV"
  * Output: 4
  * Example 3:
- *
+ * <p>
  * Input: s = "IX"
  * Output: 9
  * Example 4:
- *
+ * <p>
  * Input: s = "LVIII"
  * Output: 58
  * Explanation: L = 50, V= 5, III = 3.
  * Example 5:
- *
+ * <p>
  * Input: s = "MCMXCIV"
  * Output: 1994
  * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 1 <= s.length <= 15
  * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
  * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
@@ -117,5 +98,4 @@ public class N0013_RomanToInteger {
  * 1,010,373
  * Submissions
  * 1,764,318
- *
- * */
+ */
