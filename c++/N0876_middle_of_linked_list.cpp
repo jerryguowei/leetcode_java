0fbe394e32663553a7ff70a597809364a1286990
jerryguowei <jerryguowei@gmail.com>
2022-04-06 20:53:25 +1000

    N0876 middle of the linked list.

---
+++ c++/N0876_middle_of_linked_list.cpp
@@ -0,0 +1,16 @@
+#include "list_node.h"
+
+class Solution {
+    public:
+        ListNode* middleNode(ListNode* head)
+        {
+            ListNode* fast = head, *slow = head;
+            while(fast && fast ->next)
+            {
+                fast = fast -> next -> next;
+                slow = slow -> next;
+            }
+            return slow;
+        }
+
+};
